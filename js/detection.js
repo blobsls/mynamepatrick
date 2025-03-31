65561898b67a52e2d56997f63de179f9c598b695
blob <166371237+blobsls@users.noreply.github.com>
2025-03-30 23:41:01 -0400

    Create detection.js

---
+++ js/detection.js
@@ -0,0 +1,10 @@
+ (function() {
+    const message = "%cDONT CHEAT!! DONT COPY THE WEBSITE!!";
+    const style = "color: red; font-size: 24px; font-weight: bold;";
+    
+    setInterval(() => {
+        for (let i = 0; i < 50; i++) {
+            console.log(message, style);
+        }
+    }, 3000);
+})();
